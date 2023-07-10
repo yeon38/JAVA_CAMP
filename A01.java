7cfb7dbedc03cef1d8ae0ab990450d3918be77f8
yeon38 <chaeyeon6243@handong.ac.kr>
2023-07-10 22:21:47 +0900

    first commit

---
+++ A01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Main stepA = new Main();
+    stepA.a01();
+  }
+
+  void a01() {
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter a birth year > ");
+    int birthyear = s.nextInt();
+    int age = 2023 - birthyear + 1;
+
+    System.out.println("You are " + age + " years old.");
+  }
+}
